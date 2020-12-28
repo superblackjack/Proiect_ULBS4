b27d9193a54a24c9b0782db00d50538e1b147214
gabriela_preda <gabriela.preda@ulbsibiu.ro>
2020-12-28 23:32:50 +0200

    create User and Job EJB

---
+++ src/main/java/com/park/proiect_ulbs4/common/JobDetails.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.park.proiect_ulbs4.common;
+
+/**
+ *
+ * @author Asus
+ */
+public class JobDetails implements java.io.Serializable{
+    
+    private Integer id;
+    private String post;
+    private String descriere;
+    private String email;
+
+    public JobDetails(Integer id, String post, String descriere, String email) {
+        this.id = id;
+        this.post = post;
+        this.descriere = descriere;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getPost() {
+        return post;
+    }
+
+    public String getDescriere() {
+        return descriere;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    
+    
+}
---
+++ src/main/java/com/park/proiect_ulbs4/ejb/UserBean.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.park.proiect_ulbs4.ejb;
+
+import com.park.proiect_ulbs4.common.JobDetails;
+import com.park.proiect_ulbs4.common.UserDetails;
+import com.park.proiect_ulbs4.entity.Job;
+import com.park.proiect_ulbs4.entity.User;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+import javax.ejb.EJBException;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author Asus
+ */
+@Stateless
+public class UserBean {
+
+   private static final Logger LOG = Logger.getLogger(UserBean.class.getName());
+
+    @PersistenceContext
+    private EntityManager em;
+    
+    public List<UserDetails> getAllUsers() {
+        LOG.info("getAllUsers");
+        try {
+            Query query=em.createQuery("SELECT u FROM User u");
+            List<User> users = (List<User>) query.getResultList();
+            return copyUsersToDetails(users);
+        } catch (Exception ex) {
+            throw new EJBException(ex);
+        }
+    }
+    private List<UserDetails> copyUsersToDetails(List<User> users) {
+        List<UserDetails> detailsList = new ArrayList<>();
+        for (User user : users) {
+            UserDetails UserDetails = new UserDetails(user.getId(),
+                    user.getNume(),
+                    user.getPrenume(),
+                    user.getEmail());
+            detailsList.add(UserDetails);
+        }
+        return detailsList;
+    }
+}
